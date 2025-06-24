ab21f99add57ec09a9e67459bd2a21812cada29b
Aday25 <aday.alvarez84@gmail.com>
2025-06-24 15:23:48 +0200

    smoke effect on the cursor

--- src/js/historia.js
+++ src/js/historia.js
@@ -7,3 +7,17 @@
       }, index * 300);
     });
   }
+
+ document.addEventListener("mousemove", function(e) {
+    const trail = document.createElement("div");
+    trail.className = "cursor-trail";
+
+    trail.style.left = `${e.clientX}px`;
+    trail.style.top = `${e.clientY}px`;
+
+    document.body.appendChild(trail);
+
+    setTimeout(() => {
+      trail.remove();
+    }, 600); // igual que la duración de la animación
+  });

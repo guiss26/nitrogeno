bd6341ae41c783d1c1ab4444c52e8288be48d8a8
ambalari <larambartsumian@gmail.com>
2025-06-24 13:57:24 +0200

    collapse-final

--- src/js/juego.js
+++ src/js/juego.js
@@ -1,29 +1,8 @@
-const coll1 = document.getElementsByClassName("collapsible1");
+const collapsibles = document.querySelectorAll(".collapsible1, .collapsible2");
 
-for (let i = 0; i < coll1.length; i++) {
-    coll1[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        const content1 = this.nextElementSibling;
-        if (content1.style.display === "block") {
-            content1.style.display = "none";
-        } else {
-            content1.style.display = "block";
-        }
+collapsibles.forEach(button => {
+    button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+        content.classList.toggle("active");
     });
-}
-
-const coll2 = document.getElementsByClassName("collapsible2");
-
-for (let i = 0; i < coll2.length; i++) {
-    coll2[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        const content2 = this.nextElementSibling;
-        if (content2.style.display === "block") {
-            content2.style.display = "none";
-        } else {
-            content2.style.display = "block";
-        }
-    });
-}
+});
